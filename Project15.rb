c47e784adf6ee5698c35b79fdd7c0099248053b4
Sedclerk <seclerk@gmail.com>
2013-09-05 00:31:40 +0300

    Final working Project15
    Thank you

--- Project15.rb
+++ Project15.rb
@@ -1,50 +1,61 @@
 class Account
-   attr_accessor :balance, :account_holder
-
-def initialize
+   attr_accessor :account_holder, :balance
+  def initialize
     @balance = 0
-end
-
-def empty?
+  end
+ 
+  def empty?
     @balance == 0
   end
+ 
+  def deposit(money) 
+    if money.is_a? String
+      money = money.sub(/\$/, "").to_f
+    end
+    if money > 0        
+      @balance += money
+      puts "Deposited $#{money}. Your new balance is $#{'%.2f' % @balance}"
+    else 
+      puts " Error, you added a negative sign , remove it and try again . Thanks"
+    end
+    
+  end
+ 
+  def withdrawal(money) 
+    if money.is_a? String
+      money = money.sub(/\$/, "").to_f
+    end
+    if money > 0        
+      @balance -= money
+      puts "You took $#{money} amount. Your new balance is $#{'%.2f' % @balance}"
 
-def deposit(money)
-  if money.is_a? String
-    money = money.sub(/\$/, "").to_f
+    else 
+      puts " Error, you added a negative sign , remove it and try again . Thanks"
+    end
+      
   end
-  if money > 0 && money !~ /\w+/
-    @balance += money
-  else
-    puts "Error: Invalid Input on Deposit"
-  end
-end
-def withdraw(money)
-   if money.is_a? String
-    money = money.sub(/\$/, "").to_f
-   end
-   if money > 0 && @balance >= money && money !~ /\w+/
-    @balance -= money
-   else
-    puts "Error: Invalid Input on Withdrawal"
-   end
-end
-def transfer(money, other_client)
-  if money.is_a? String
-    money = money.sub(/\$/, "").to_f
-  end
-  if money > 0 && @balance >= money && money !~ /\w+/ && defined? other_client 
-    @balance -= money
-    other_client.deposit(money)
-  else
-    puts "Error: Invalid Input on Transfer to Client:#{other_client}"
+ 
+  def transfer(money, other_client) 
+    if money.is_a? String
+      money = money.sub(/\$/, "").to_f
+    end
+    if money > 0   
+      @balance -= money       
+	    other_client.deposit(money)
+      puts "Deposited $#{money} to #{other_client.account_holder}'s account. Your current balance  is $#{'%.2f' % @balance}"
+    else 
+      puts " Error, you added a negative sign , remove it and try again . Thanks"
+    end
+
   end
  end
-def num_clients()
-  return @@num_clients
-end
-def print_account()
-  puts "#{@account_holder} ---- #{'%.2f' % @balance}"
-end
-end
 
+a = Account.new
+a.account_holder = 'Steve'
+a.deposit('$40.45')
+a.withdrawal('$20.15')
+
+b = Account.new
+b.account_holder = 'Jeron'
+b.deposit('$100.25')
+a.transfer('$20.25', b)
