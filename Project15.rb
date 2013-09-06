e6cd8484b6d45d8765cf62f1d21daf4283c92bb7
Sedclerk <seclerk@gmail.com>
2013-09-06 21:30:49 +0300

    last

--- Project15.rb
+++ Project15.rb
@@ -1,61 +1,118 @@
 class Account
-   attr_accessor :account_holder, :balance
-  def initialize
-    @balance = 0
-  end
- 
-  def empty?
-    @balance == 0
-  end
- 
-  def deposit(money) 
-    if money.is_a? String
-      money = money.sub(/\$/, "").to_f
-    end
-    if money > 0        
-      @balance += money
-      puts "Deposited $#{money}. Your new balance is $#{'%.2f' % @balance}"
-    else 
-      puts " Error, you added a negative sign , remove it and try again . Thanks"
+    
+    attr_accessor :account_holder, :balance
+    
+    def initialize (name)
+        
+        @balance = 0
+        
+        @account_holder = name
+        
+        if name == nil
+            
+            puts "Invalid input , that is not a name my friend"
+            
+            else
+            
+            puts "#{@account_holder}"
+            
+        end
+        
     end
     
-  end
- 
-  def withdrawal(money) 
-    if money.is_a? String
-      money = money.sub(/\$/, "").to_f
+    def deposit(money)
+        
+        if money.is_a? String
+            
+            money = money.sub(/\$/, "").to_f
+            
+        end
+        
+        if money > 0
+            
+            @balance += money
+            
+            puts "Deposited $#{money}. Your new balance is $#{'%.2f' % @balance}"
+            
+            else
+            
+            puts "Error: Invalid Input on Deposit"
+            
+        end
+        
     end
-    if money > 0        
-      @balance -= money
-      puts "You took $#{money} amount. Your new balance is $#{'%.2f' % @balance}"
+    
+    def withdrawal(money)
+        
+        if money.is_a? String
+            
+            money = money.sub(/\$/, "").to_f
+            
+        end
+        
+        if money > 0 && money <= @balance
+            
+            @balance -= money
+            
+            puts "You took $#{money} amount. Your new balance is $#{'%.2f' % @balance}"
+            
+            elsif money > @balance
+            
+            puts " Sorry, you are broke. You only have this $#{'%.2f' % @balance} left on your account"
+            
+            else
+            
+            puts "Error: Invalid Input on Withdrawal"
+            
+        end
+        
+    end
+    
+    def transfer(money, other_client)
+        
+        if money.is_a? String
+            
+            money = money.sub(/\$/, "").to_f
+            
+        end
+        
+        if money > 0 && money <= @balance
+            
+            @balance -= money
+            
+            other_client.deposit(money)
+            
+            puts "Deposited $#{money} to #{other_client.account_holder}'s account. Your current balance  is $#{'%.2f' % @balance}"
+            
+            elsif money > @balance
+            
+            puts " Sorry, you are broke. You only have this $#{'%.2f' % @balance} left on your account"
+            
+            else
+            puts "Error: Invalid Input on Transfer to Client:#{other_client}"
+        end
+    end
+    
+end
 
-    else 
-      puts " Error, you added a negative sign , remove it and try again . Thanks"
-    end
-      
-  end
- 
-  def transfer(money, other_client) 
-    if money.is_a? String
-      money = money.sub(/\$/, "").to_f
-    end
-    if money > 0   
-      @balance -= money       
-	    other_client.deposit(money)
-      puts "Deposited $#{money} to #{other_client.account_holder}'s account. Your current balance  is $#{'%.2f' % @balance}"
-    else 
-      puts " Error, you added a negative sign , remove it and try again . Thanks"
-    end
+a = Account.new ('Steve')
 
-  end
- end
-
-a = Account.new
-a.account_holder = 'Steve'
 a.deposit('$40.45')
+
 a.withdrawal('$20.15')
 
-b = Account.new
-b.account_holder = 'Jeron'
+b = Account.new ('Jeron')
+
 b.deposit('$100.25')
+
 a.transfer('$20.25', b)
+
+a.transfer('$30.25', b)
+
+a.withdrawal('$50')
+
+a.deposit('cows')
+
+c = Account.new (nil)
+
+a.withdrawal(-55)
